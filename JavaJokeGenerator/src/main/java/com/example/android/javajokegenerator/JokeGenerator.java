38ba117a6e8da33f381a8520301ae0aa0782bb00
ssantos14 <sylvana.e.santos@gmail.com>
2018-03-18 01:29:11 -0700

    Change java joke generator

--- JavaJokeGenerator/src/main/java/com/example/android/javajokegenerator/JokeGenerator.java
+++ JavaJokeGenerator/src/main/java/com/example/android/javajokegenerator/JokeGenerator.java
@@ -1,7 +1,19 @@
 package com.example.android.javajokegenerator;
 
+import java.util.Random;
+
 public class JokeGenerator {
     public String getJoke(){
-        return "Why do cows have hooves instead of feet? Because they lack toes";
+        int numberOfJokes = Jokes.length;
+        Random rand = new Random();
+        int jokeIndex = rand.nextInt(numberOfJokes);
+        return Jokes[jokeIndex];
     }
+
+    private String[] Jokes = {
+            "Which of King Arthur's knights built the round table? Sir Cumfrence",
+            "Why do cows have hooves instead of feet? Because they lack toes",
+            "What happens to frogs when they park illegally? They get toad",
+            "Why did the Norwegian Navy put barcodes on their ships? So that when they dock they can Scandinavian"
+    };
 }
